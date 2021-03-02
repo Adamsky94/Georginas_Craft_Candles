3e90cf1c96b3a8092bd42cdaeadc2c2fe7d189bb
Adamsky94 <a.kiss1994@gmail.com>
2021-03-02 12:43:41 +0000

    Added products and categoires model

--- ginas_candles/products/models.py
+++ ginas_candles/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    scent =  models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    material = models.CharField(max_length=20)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    promotion = models.BooleanField(default=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
