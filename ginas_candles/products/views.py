697d27a3a11d7b40d48fb4a4d07870830127e21a
Adamsky94 <a.kiss1994@gmail.com>
2021-03-06 10:53:19 +0000

    Added products

--- ginas_candles/products/views.py
+++ ginas_candles/products/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import Product, Category
+from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to return all products """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
 
