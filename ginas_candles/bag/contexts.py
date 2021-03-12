552939ab9a386d5c4c684650a0107515d4ca9b8a
Adamsky94 <a.kiss1994@gmail.com>
2021-03-12 17:05:50 +0000

    Added qty input box to product-detail and shopping bag pages

--- ginas_candles/bag/contexts.py
+++ ginas_candles/bag/contexts.py
@@ -10,20 +10,20 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
-
-        delivery = 0
-
+    for item_id, item_data in bag.items():
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+    
+    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
