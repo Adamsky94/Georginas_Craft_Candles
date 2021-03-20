cbffd8f10cb9950bec4c0d1342fa7b6fad7a929d
Adamsky94 <a.kiss1994@gmail.com>
2021-03-20 12:14:17 +0000

    Added calc subtotal template filter

--- ginas_candles/bag/contexts.py
+++ ginas_candles/bag/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
 
     bag_items = []
@@ -11,6 +12,7 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
@@ -19,11 +21,11 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-    
+
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
