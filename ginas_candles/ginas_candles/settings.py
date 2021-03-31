d69d14a4afe488f8d5cd86110142eb980d7d0e22
Adamsky94 <a.kiss1994@gmail.com>
2021-03-31 13:10:15 +0000

    Added Heroku secret key

--- ginas_candles/ginas_candles/settings.py
+++ ginas_candles/ginas_candles/settings.py
@@ -31,8 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost' , 'ginas-candles.herokuapp.com']
+ALLOWED_HOSTS = ['ginas-candles.herokuapp.com', 'localhost']
 
+HEROKU_SECRET_KEY = os.environ.get('HEROKU_SECRET_KEY')
 
 # Application definition
 
