9408b81f61abd966a784f532f7a3b1a4b38079d7
Adamsky94 <a.kiss1994@gmail.com>
2021-03-31 12:05:20 +0000

    Starting postgres db on Heroku with help from Fatima from TutorTeam

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ginas-candles.wsgi:application
--- ginas_candles/ginas_candles/settings.py
+++ ginas_candles/ginas_candles/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 from decouple import config
 from dotenv import load_dotenv
@@ -79,7 +80,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
@@ -123,12 +124,17 @@ WSGI_APPLICATION = 'ginas_candles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
